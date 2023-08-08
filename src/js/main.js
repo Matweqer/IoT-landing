3e00ea98038eb6cbcc132347faa5edd09bd59505
matweqer <matweqer@mail.ru>
2023-08-09 00:31:47 +0300

    Added block contact us

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,12 @@ import 'slick-carousel';
 
 import animationMap from './animation-map';
 import bodyResizeObserver from './body-resize-observer';
+import contactUs from './blocks/contact-us';
+import scrollToSection from "./scroll-to-section";
 
 $(document).ready(() => {
+    contactUs.init();
+    scrollToSection.init();
     animationMap.init();
     bodyResizeObserver.init();
 });
