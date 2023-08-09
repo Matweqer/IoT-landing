2494e0681960850a37b1e6c22703ebe834097159
matweqer <matweqer@mail.ru>
2023-08-10 00:25:45 +0300

    Added mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -6,11 +6,20 @@ import bodyResizeObserver from './body-resize-observer';
 import contactUs from './blocks/contact-us';
 import scrollToSection from './scroll-to-section';
 import navigation from './blocks/navigation';
+import { breakpoints, matchMedia } from './utils/match-media';
+import loadImages from './load-images';
 
 $(document).ready(() => {
-    navigation.init()
     contactUs.init();
-    scrollToSection.init();
-    animationMap.init();
-    bodyResizeObserver.init();
+
+    if (matchMedia.from(breakpoints.tabletHorizontal)) {
+        navigation.init()
+        scrollToSection.init();
+        animationMap.init();
+        bodyResizeObserver.init();
+    }
+
+    if (matchMedia.till(breakpoints.tabletHorizontal)) {
+        loadImages(document.body);
+    }
 });
