e0fcdd4f6277bc513864504bd68e517e708b87af
matweqer <matweqer@mail.ru>
2023-10-03 23:44:47 +0300

    Added additional info on sending contact-us form

--- src/js/email.js
+++ src/js/email.js
@@ -3,7 +3,7 @@ import {emailConfig} from './config/emailConfig'
 const emailHost = emailConfig.EMAIL_HOST
 const emailApi = '/api/v1/support'
 export async function sendMail(data) {
-    return await fetch(`${emailHost}${emailApi}`, {
+    const response = await fetch(`${emailHost}${emailApi}`, {
         method: 'POST',
         body: data,
         mode: 'no-cors'
